f11e419912bcc859e706154e5c80bb447145eacc
Per1sha <aiperiyusupova1@gmail.com>
2024-05-27 05:03:14 +0300

    Add files via upload

---
+++ H 1/script.js
@@ -0,0 +1,7 @@
+function Check()
+{
+    if(document.Sel3.Pwd.value==document.Sel3.Pwd.value)
+        alert("Вас зарегестрировали\nID="+document.Sel3.Id.value+"\nPassword="+document.Sel3.Pwd.value);
+    else
+        alert("Пароли не совпадают\nПопробуйте еще раз");
+}
